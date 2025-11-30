9f6454704f1a5934a01110af582b5b911b269e09
Ayush kumar <ayush@Ayushs-MacBook-Air-3.local>
2025-11-30 21:30:30 +0530

    Initial Commit ums backend project setup

---
+++ src/main/java/com/ums/backend/UmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ums.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmsBackendApplication.class, args);
+	}
+
+}
